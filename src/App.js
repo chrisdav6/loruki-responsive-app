6b64a474c929e8a388dcdb70e2d30186f8115d15
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-01-21 14:05:53 -0500

    Setup routing for additional pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Features from './pages/Features';
+import Docs from './pages/Docs';
 
 const App = () => {
   return (
     <div className='app'>
       <Switch>
-        <Route to='/' exact>
+        <Route path='/' exact>
           <Home />
+        </Route>
+        <Route path='/features'>
+          <Features />
+        </Route>
+        <Route path='/docs'>
+          <Docs />
         </Route>
       </Switch>
     </div>
---
+++ src/pages/Docs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Docs = () => {
+  return <div className='docs'>Docs</div>;
+};
+
+export default Docs;
